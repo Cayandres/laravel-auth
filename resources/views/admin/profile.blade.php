b1c820b9603408b127ab339ea069bcc9ddda1e79
Andres Cayambe <andrescayambe99@hotmail.com>
2023-06-15 16:56:55 +0200

    [add]profile-card

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container text-center">
+    <div class="card" style="width: 20rem;">
+      <div class="card-body">
+        <h5 class="card-title">Profilo Utente</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">nome: {{ Auth::user()->name }}</h6>
+        <p class="card-text">email :{{ Auth::user()->email }}</p>
+      </div>
+    </div>
 
-<h1>Questa è la tua dashboard</h1>
+</div>
 
 @endsection
